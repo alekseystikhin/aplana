fd9dc622e1de833f6a53f1ee1c9edfe59b55ca8c
aleksey stikhin <stikhinsky@gmail.com>
2020-08-20 14:36:03 +0300

    ex.5 Таблица умножения

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.next());
+        System.out.println("Result:");
+        System.out.println(number);
+        System.out.println(number*2);
+        System.out.println(number*3);
+        System.out.println(number*4);
+        System.out.println(number*5);
+        System.out.println(number*6);
+        System.out.println(number*7);
+        System.out.println(number*8);
+        System.out.println(number*9);
+        System.out.println(number*10);
+    }
+}
