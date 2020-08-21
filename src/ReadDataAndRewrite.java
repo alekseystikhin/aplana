59f74272ac71a115c24784275c7f046a26aa78c9
aleksey stikhin <stikhinsky@gmail.com>
2020-08-21 19:49:05 +0300

    ex.18 Выгрузка данных из файла, проверка и перезапись

---
+++ src/ReadDataAndRewrite.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadDataAndRewrite {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStringsFromFile = 0;
+        int numberOfStringsFromInput = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("resources\\test3.txt")))) {
+            String s;
+            while ((s = br.readLine()) != null) {
+                ++numberOfStringsFromFile;
+                System.out.println(s);
+            }
+
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+        String str = "";
+        String temp;
+        System.out.println("Признак конца ввода - строка \"stop\"");
+        do {
+            temp = scanner.nextLine();
+            if ("stop".compareTo(temp) == 0) break;
+            str += temp + "\r\n";
+            ++numberOfStringsFromInput;
+
+        } while ("stop".compareTo(temp) != 0);
+
+        if (numberOfStringsFromFile == numberOfStringsFromInput) {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File("resources\\test3.txt")))) {
+                bw.write(str);
+            } catch (IOException ex) {
+
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
