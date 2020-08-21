27c96e7d90485aa8148f7d86420f4a3b3f1e2650
aleksey stikhin <stikhinsky@gmail.com>
2020-08-21 15:59:27 +0300

    ex.11 Ввод строки с клавиатуры

---
+++ src/EnterString.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class EnterString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.next();
+        System.out.print(temp);
+    }
+}
